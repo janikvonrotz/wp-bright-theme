1bf4223f344f6ba1d3599c383a7ebe600f24cbf5
Janik von Rotz <contact@janikvonrotz.ch>
2013-09-25 12:16:02 +0200

    chilp

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,37 @@
 module.exports = function(grunt){
 
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
+
+		less: {
+			production: {
+				files: {
+					"css/style.css": "less/style.less"
+				}
+			}
+		},
+	
 		cssmin: {
-		combine: {
-			files: {
-				'cd $psp/to/output.css': ['path/to/input_one.css', 'path/to/input_two.css']
+			combine: {
+				files: {
+					'style.css': [
+						'css/wp-header.css',
+						'components/bootstrap/docs/assets/css/bootstrap.css', 
+						'components/bootstrap/docs/assets/css/bootstrap-responsive.css',
+						'css/icon-fonts/metro/style.css',
+						'css/fonts/raleway_thin/stylesheet.css',
+						'css/fonts/junction/stylesheet.css',
+						'css/colors/brand/style.css',
+						'css/style.css'
+					]
+				}
 			}
 		}
-}
+	
 	});
 
+	grunt.registerTask('default', ['less', 'cssmin']);
+	
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
-
+	grunt.loadNpmTasks('grunt-contrib-less');
+	
 };
